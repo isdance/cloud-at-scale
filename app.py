7c4f68f72af69e2224aa16a764e40cee622f6d32
isdance <isdance2004@hotmail.com>
2020-05-11 07:36:08 +0000

    minor

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-def myfunc():
-    return 1
+from flask import Flask, escape, request
 
-print(myfunc())
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
